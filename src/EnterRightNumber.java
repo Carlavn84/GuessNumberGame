42c973b64dd0ec03b1af6c604fca47b16467f109
Carla <carla.vo@liones.nl>
2020-07-03 14:37:57 +0200

    Add player name

--- src/EnterRightNumber.java
+++ src/EnterRightNumber.java
@@ -5,29 +5,47 @@ public class EnterRightNumber {
     public static void main(String[] args) {
         System.out.println("Please get my number from 1 to 100 ");
 
+//       Create a random number which is the number players have to guess
         int min = 1;
         int max = 100;
         Random randomNumber = new Random();
         int value = min + randomNumber.nextInt(max);
 
+//        Create an object to store player inputs
         Scanner input = new Scanner(System.in);
 
-        int attemptCount = 1;
-        while (attemptCount > 0) {
-            System.out.println("Enter your number:");
-            int numberInput = input.nextInt();
+        boolean playerState = false;
+        System.out.println("Enter your name:");
+        String playerName = input.nextLine();
 
-            if (value == numberInput) {
-                System.out.println("The number is correct. You have" + " " + attemptCount + " " + "attempt(s)." + "Well done, dude!!!");
-                return;
-            } else {
-                if (value >= numberInput) {
-                    System.out.println("The number is higher. Try again!!!");
+        while (playerName == null || playerName.isEmpty()) {
+            System.out.println("You have to enter your name to start the game");
+            System.out.println("Enter your name:");
+            String playerNameAGain = input.nextLine();
+             playerName = playerNameAGain;
+           }
+
+       if (playerName != null && !playerName.isEmpty()) {
+            System.out.println("Game start");
+            int attemptCount = 1;
+            while (attemptCount > 0) {
+                System.out.println("Enter your number:");
+                int numberInput = input.nextInt();
+
+                if (value == numberInput) {
+                    System.out.println("The number is correct. You have" + " " + attemptCount + " " + "attempt(s)." + "Well done," + " " + playerName + "!!!");
+                    return;
                 } else {
-                    System.out.println("The number is less. Try again!!!");
+                    if (value >= numberInput) {
+                        System.out.println("The number is higher. Try again!!!");
+                    } else {
+                        System.out.println("The number is less. Try again!!!");
+                    }
                 }
+                attemptCount += 1;
             }
-            attemptCount += 1;
         }
+
+
     }
 }
